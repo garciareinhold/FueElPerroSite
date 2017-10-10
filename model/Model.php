0e16534e690b25b19b0316a2aaeab17fa7c6dde7
luchontandil <luchontandil@hotmail.com>
2017-10-10 19:33:12 -0300

    Bugfix: coneccion despues de la creacion de la db

--- model/Model.php
+++ model/Model.php
@@ -7,13 +7,16 @@ class Model
   function __construct()
   {
     try {
+
       $this->db = new PDO('mysql:host=localhost;'
       .'dbname=fueelperro_db;charset=utf8'
       , 'root', '');
     } catch (Exception $e) {
-      $this->model = new dbModel();
       $this->db = dbModel::getInstance();
       $this->db->buildDBfromFile("fueelperro_db");
+      $this->db = new PDO('mysql: host=localhost;'
+      .'dbname=fueelperro_db;charset=utf8'
+      , 'root', '');
     }
   }
 }
