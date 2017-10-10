0bf7beff326d2d808ec935999593fe2884a18583
luchontandil <luchontandil@hotmail.com>
2017-10-09 22:19:28 -0300

    Deploy de la base de datos automatica si es que no se encuentra una

--- model/Model.php
+++ model/Model.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'model/dbModel.php';
 
 class Model
 {
@@ -6,9 +7,15 @@ class Model
 
   function __construct()
   {
-    $this->db = new PDO('mysql:host=localhost;'
-    .'dbname=fueelperro_db;charset=utf8'
-    , 'root', '');
+    try {
+      $this->db = new PDO('mysql:host=localhost;'
+      .'dbname=fueelperro_db;charset=utf8'
+      , 'root', '');
+    } catch (Exception $e) {
+      $this->model = new dbModel();
+      $this->db = dbModel::getInstance();
+      $this->db->buildDBfromFile("fueelperro_db");
+    }
   }
 }
  ?>
