d806cf508e20e3eddc96aaacdd9efc81e70b6693
Arturo Garcia Reinhold <arturogreinhold@gmail.com>
2017-10-10 03:04:06 -0300

    Subimos el route.php

--- model/Model.php
+++ model/Model.php
@@ -1,14 +1,19 @@
 <?php
-
+include_once 'model/dbModel.php';
 class Model
 {
   protected $db;
-
   function __construct()
   {
-    $this->db = new PDO('mysql:host=localhost;'
-    .'dbname=fueelperro_db;charset=utf8'
-    , 'root', '');
+    try {
+      $this->db = new PDO('mysql:host=localhost;'
+      .'dbname=fueelperro_db;charset=utf8'
+      , 'root', '');
+    } catch (Exception $e) {
+      $this->model = new dbModel();
+      $this->db = dbModel::getInstance();
+      $this->db->buildDBfromFile("fueelperro_db");
+    }
   }
 }
  ?>
---
+++ model/dbModel.php
@@ -0,0 +1,50 @@
+<?php
+class dbModel
+{
+private $_connection;
+private static $_instance;
+public static function getInstance(){
+  if(!self::$_instance){
+    self::$_instance = new self();
+  }
+  return (self::$_instance);
+}
+function __construct(){
+  try {
+    $this->_connection = new PDO('mysql:host=localhost',"root","");
+  }
+  catch (Exception $e) {
+    echo "ERROR CREANDO LA BASE DE DATOS ".$e;
+  }
+}
+public function buildDBfromFile($dbName){
+  try {
+      $this->_connection = new PDO('mysql:host=localhost;',"root","");
+      $this->_connection->exec('CREATE DATABASE IF NOT EXISTS '.$dbName );
+      $this->_connection->exec('USE '.$dbName);
+      $queries = $this->loadSQLSchema('fueelperro_db.sql');
+      $i=0;
+      while($i<count($queries)&&strlen($this->_connection->errorInfo()[2] == 0)){
+        $this->_connection->exec($queries[$i]);
+        $i++;
+      }
+  }
+  catch (Exception $e) {
+    echo $e;
+  }
+}
+function loadSQLSchema($nombre){
+  $querys = fopen($nombre, "r+");
+  $sql = "";
+  while (!feof($querys)) {
+    $linea = fgets($querys);
+    $linea = str_replace("'", '"', $linea);
+    if(!preg_match('/--/', $linea))   $sql .= $linea;
+  }
+  fclose($querys);
+  $querys = explode(";", $sql);
+  unset($querys[count($querys)-1]);
+  return $querys;
+}
+}
+ ?>
