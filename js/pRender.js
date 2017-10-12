606a9fec37347854555afb6f2c23854ae4f826b1
Arturo Garcia Reinhold <arturogreinhold@gmail.com>
2017-10-12 07:51:08 -0300

    Terminamos de limpiar el pRender.js de codigo comentado

--- js/pRender.js
+++ js/pRender.js
@@ -5,128 +5,96 @@ $(document).ready(function(){
     $.post("editarCategoria",data,adminMostrarAjax);
   }
   function mostrarEditarCat(data) {
-    // console.log("entre en editar");
     let id_categoria= {id: data};
     $.post("mostrarEditarCategoria",id_categoria,adminMostrarAjax);
   }
-
   function agregarCat(data) {
-    // console.log("entre en agregar");
     $.post("agregarCategoria",data,adminMostrarAjax);
   }
-
   function borrarCat(data) {
-    // console.log("entre en borrar");
     let id_categoria= {id: data};
     $.post("borrarCategoria",id_categoria,adminMostrarAjax);
   }
   //ABM productos
-    function editarProd(data) {
-      // console.log(data);
-      $.post("editarProducto",data,adminMostrarAjax);
-    }
-    function mostrarEditarProd(data) {
-      // console.log("entre en mostrarEditarProd");
-      let id_delantal= {id: data};
-      $.post("mostrarEditarProducto",id_delantal,adminMostrarAjax);
-    }
+  function editarProd(data) {
+    $.post("editarProducto",data,adminMostrarAjax);
+  }
+  function mostrarEditarProd(data) {
+    let id_delantal= {id: data};
+    $.post("mostrarEditarProducto",id_delantal,adminMostrarAjax);
+  }
+  function agregarProducto(data) {
+    $.post("agregarProd",data,adminMostrarAjax);
+  }
+  function borrarProd(data) {
+    let id_producto= {id: data};
+    $.post("borrarProducto",id_producto,adminMostrarAjax);
+  }
+  function listarProductosCategoria(data){
+    let id_categoria={id: data};
+    $.post("delantalesCategoria",id_categoria,adminMostrarAjax);
+  }
+  function mostrarDetalle(data) {
+    let id_delantal={id: data};
+    $.post("delantal",id_delantal,adminMostrarAjax);
+  }
 
-    function agregarProducto(data) {
-      // console.log("entre en la funcion");
-      $.post("agregarProd",data,adminMostrarAjax);
-    }
-
-    function borrarProd(data) {
-      let id_producto= {id: data};
-      $.post("borrarProducto",id_producto,adminMostrarAjax);
-    }
-    function listarProductosCategoria(data){
-      let id_categoria={id: data};
-      $.post("delantalesCategoria",id_categoria,adminMostrarAjax);
-    }
-    function mostrarDetalle(data) {
-      // console.log("entre en la funcion");
-      let id_delantal={id: data};
-      $.post("delantal",id_delantal,adminMostrarAjax);
-    }
-    // function autenticar(data) {
-    //   console.log(data);
-    //   $.post("autenticacion",data,function(result){
-    //     console.log(result);
-    //   });
-    // }
-
-// Partial Render
-  function adminMostrarAjax(result)
-    {
-      // console.log("entre en el mostrarAjax");
-      // console.log(result);
+// Function para mostar Contenido de ajax y bindear funciones
+  function adminMostrarAjax(result) {
       $("#js-pRender").html(result);
-        $( ".editarCat" ).on( "click", function( event ) {
-          event.preventDefault();
-          let data = $(this).data("id");
-          mostrarEditarCat(data);
-        });
-        // $(".loginForm").on( "submit", function( event ) {
-        //   event.preventDefault();
-        //   console.log("entre en el binding");
-        //   let data = $(this).serialize();
-        //   console.log(data);
-        //   autenticar(data);
-        // });
-        // $( ".elegirCategoria" ).on( "click", function( event ) {
-        //   event.preventDefault();
-        //   let data = $(this).val();
-        //   $("#categoria").val(data);
-        // });
-        $( ".borrarCat" ).on( "click", function( event ) {
-          event.preventDefault();
-          let data = $(this).data("id");
-          borrarCat(data);
-        });
-        $( "#agregarCat" ).on( "submit", function( event ) {
+
+      $( ".editarCat" ).on( "click", function( event ) {
+        event.preventDefault();
+        let data = $(this).data("id");
+        mostrarEditarCat(data);
+      });
+      $( ".borrarCat" ).on( "click", function( event ) {
+        event.preventDefault();
+        let data = $(this).data("id");
+        borrarCat(data);
+      });
+      $( "#agregarCat" ).on( "submit", function( event ) {
+        event.preventDefault();
+        let data = $(this).serialize();
+        agregarCat(data);
+      });
+      $( ".editarCategorias" ).on( "submit", function( event ) {
+        event.preventDefault();
+        let data = $(this).serialize();
+        editarCat(data);
+      });
+      $( ".editarProd" ).on( "click", function( event ) {
+        event.preventDefault();
+        let data = $(this).data("id");
+        mostrarEditarProd(data);
+      });
+      $( ".borrarProd" ).on( "click", function( event ) {
+        event.preventDefault();
+        let data = $(this).data("id");
+        borrarProd(data);
+      });
+      $( "#agregarProd" ).on( "submit", function( event ) {
+        event.preventDefault();
+        let data = $(this).serialize();
+        agregarProducto(data);
+      });
+      $( ".editarDelantales" ).on( "submit", function( event ) {
           event.preventDefault();
           let data = $(this).serialize();
-          agregarCat(data);
-        });
-        $( ".editarCategorias" ).on( "submit", function( event ) {
-            event.preventDefault();
-            let data = $(this).serialize();
-            editarCat(data);
-          });
-            $( ".editarProd" ).on( "click", function( event ) {
-              event.preventDefault();
-              let data = $(this).data("id");
-              mostrarEditarProd(data);
-            });
-            $( ".borrarProd" ).on( "click", function( event ) {
-              event.preventDefault();
-              let data = $(this).data("id");
-              borrarProd(data);
-            });
-            $( "#agregarProd" ).on( "submit", function( event ) {
-              event.preventDefault();
-              let data = $(this).serialize();
-              // console.log("entre en el binding");
-              agregarProducto(data);
-            });
-            $( ".editarDelantales" ).on( "submit", function( event ) {
-                event.preventDefault();
-                let data = $(this).serialize();
-                editarProd(data);
-              });
-            $(".productosCategoria").on("click", function(event){
-              event.preventDefault();
-              let data= $(this).data("id");
-              listarProductosCategoria(data);
-            })
-            $(".detalle").on("click", function(event){
-              // console.log("entre en el binding");
-              event.preventDefault();
-              let data= $(this).data("id");
-              mostrarDetalle(data);
-            })
+          editarProd(data);
+      });
+      $(".productosCategoria").on("click", function(event){
+        event.preventDefault();
+        let data= $(this).data("id");
+        listarProductosCategoria(data);
+      })
+      $(".detalle").on("click", function(event){
+        event.preventDefault();
+        let data= $(this).data("id");
+        mostrarDetalle(data);
+      });
   }
+
   $(".navegacion").on("click", function (event) {
     event.preventDefault();
     let seccion_pagina=$(this).attr("id");
@@ -134,6 +102,6 @@ $(document).ready(function(){
         url: seccion_pagina,
         success: adminMostrarAjax
       })
-      });
+  });
 
 });
