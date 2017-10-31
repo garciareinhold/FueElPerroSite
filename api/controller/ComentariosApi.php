58e68fd9416fe1708e0e887d056889323e5a25de
Arturo Garcia Reinhold <arturogreinhold@gmail.com>
2017-10-31 18:29:39 -0300

    Agregamos el esqueleto de la API

---
+++ api/controller/ComentariosApi.php
@@ -0,0 +1,5 @@
+<?php
+  require_once('../model/ComentariosModel.php');
+  require_once('Api.php');
+
+?>
---
+++ api/route.php
@@ -0,0 +1,30 @@
+<?php
+
+define('RESOURCE', 0);
+define('PARAMS', 1);
+
+include_once 'config/Router.php';
+include_once '../model/Model.php';
+//controllers
+
+$router = new Router();
+//url, verb, controller, method
+$router->AddRoute();
+$route = $_GET['resource'];
+$array = $router->Route($route);
+
+if(sizeof($array) == 0)
+  echo "404";
+else
+{
+  $controller = $array[0];
+  $metodo = $array[1];
+  $url_params = $array[2];
+  echo (new $controller())->$metodo($url_params);
+}
+
+
+
+
+
+?>
