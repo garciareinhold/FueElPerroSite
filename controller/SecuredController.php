3833e71fb222e002f21f635fd3ac8f4853745124
Arturo Garcia Reinhold <arturogreinhold@gmail.com>
2017-11-15 15:07:04 -0300

    Arreglamos un detalle del controller

--- controller/SecuredController.php
+++ controller/SecuredController.php
@@ -6,14 +6,16 @@ class SecuredController extends Controller
   function __construct()
   {
     session_start();
-    if(isset($_SESSION['USER'])){
+    if(isset($_SESSION['USER'])&&!empty($_SESSION['USER'])){
       if ($_SESSION['ADMIN']==0) {
-        header('Location: '.HOME);
+        header('Location: '.LOGIN);
+        echo "Debes ser administrador para ingresar";
         die();
       }
     }
     else {
       header('Location: '.LOGIN);
+      echo "Debes estar registrado y conseguir el permiso para ingresar";
       die();
     }
   }
